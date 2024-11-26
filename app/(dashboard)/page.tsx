445d826c0738f77d1ec84e71af80f468b0a39934
Steven Duque <steven.122@hotmail.es>
2024-11-27 00:48:57 +0100

    api first steps

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function home(){
   return (
-    <p>
-      this is an auth-route
-    </p>
+    <p>user button is no longer neccesary</p>
   )
 
 }
